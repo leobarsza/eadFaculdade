412ac90127806cfa33ff138eb44ebca19d5709f5
Leonardo Barbosa de Souza <leonardobarsza@gmail.com>
2025-08-12 18:19:22 -0300

    matrizes

---
+++ matriz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int matA[4][4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            scanf("%d", &matA[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("%d", matA[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
