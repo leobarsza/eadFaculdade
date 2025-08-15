7b92f77ea275b759949c9ea70e74097885e59f89
Leonardo Barbosa de Souza <leonardobarsza@gmail.com>
2025-08-15 16:31:58 -0300

    feat : testes com estruturas de repetição

---
+++ for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 10; i > 0; i--)
+    {
+        printf("i: %d\n", i);
+    }
+    /*for (int i = 0; i <= 10; i++)
+    {
+        printf("i: %d\n", i);
+    }*/
+
+    printf("Fim do programa");
+    return 0;
+}
---
+++ while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    while (i > 0)
+    {
+        printf("i: %d\n", i);
+        i--;
+    }
+
+    printf("Fim do programa");
+    return 0;
+}
